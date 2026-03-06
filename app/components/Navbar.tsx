49785a276c0741e4571facef6020c6aac782a4b2
Jeremias Adrogue <lafraguacatering@gmail.com>
2026-03-06 12:08:34 -0300

    Remove 'Qué te podemos ofrecer' from dropdown and rename Asado/Pizzas

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,25 +9,22 @@ export default function Navbar() {
         </Link>
 
         <nav className="navLinks navLinksCentered" aria-label="Navegación principal">
-          <a className="navLink" href="/#nosotros">
+          <Link className="navLink" href="/#nosotros">
             Nosotros
-          </a>
+          </Link>
 
-          <a className="navLink" href="/#productos">
+          <Link className="navLink" href="/#productos">
             Productos
-          </a>
+          </Link>
 
           <details className="navDrop">
             <summary className="navLink navDropSummary">Catering</summary>
             <div className="navDropMenu" role="menu" aria-label="Catering">
-              <a className="navDropItem" href="/#oferta">
-                Qué te podemos ofrecer
-              </a>
               <Link className="navDropItem" href="/asado">
-                Asado argentino
+                Asado
               </Link>
               <Link className="navDropItem" href="/pizzas">
-                Pizzas napolitanas
+                Pizzas
               </Link>
             </div>
           </details>
